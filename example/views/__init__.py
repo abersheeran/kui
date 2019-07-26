c1723d360df88aa17c21cc9c3b47b13a69f556f3
AberSheeran <AberSheeran@qq.com>
2019-07-26 12:13:39 +0800

    update example

--- example/views/__init__.py
+++ example/views/__init__.py
@@ -5,8 +5,8 @@ from index.config import logger
 class Middleware(MiddlewareMixin):
 
     async def process_request(self, request):
-        print("enter first process request")
+        logger.info("enter first process request")
 
     async def process_response(self, request, response):
-        print("enter last process response")
+        logger.info("enter last process response")
         return response
