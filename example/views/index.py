69c31cd0911cfe6628a10e79c9d58981ecdcfa3f
abersheeran <me@abersheeran.com>
2019-08-01 00:26:04 +0800

    update example to support reload

--- example/views/index.py
+++ example/views/index.py
@@ -3,14 +3,14 @@ from index.view import View
 from index.config import logger
 from index.responses import TemplateResponse
 
-from views.utils.db import some_db_settings
+from views.utils import db
 
 
 class HTTP(View):
 
     def get(self):
         logger.info("get repsonse")
-        return TemplateResponse("home.html", {"request": self.request, "db": some_db_settings})
+        return TemplateResponse("home.html", {"request": self.request, "db": db.some_db_settings})
 
     def post(self):
         return {"message": "some error in server"}, 500, {"server": "index.py"}
