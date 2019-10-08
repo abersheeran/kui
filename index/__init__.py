61f782fd205e7d9758e1917813d4418eb5ea5fc8
abersheeran <me@abersheeran.com>
2019-10-08 15:11:01 +0800

    https://github.com/encode/uvicorn/pull/423

--- index/__init__.py
+++ index/__init__.py
@@ -6,10 +6,6 @@ from .utils import import_module
 from .config import Config
 from .core import app
 
-# use IOCP in windows
-if sys.platform == 'win32' and (sys.version_info.major >= 3 and sys.version_info.minor >= 7):
-    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
-
 import_module('commands')
 import_module('events')
 import_module('responses')
