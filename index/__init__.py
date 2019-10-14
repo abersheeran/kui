9d50dbb9ba38a99e3582d2d3842d5a529f631d0f
abersheeran <me@abersheeran.com>
2019-10-14 17:06:51 +0800

    change import order

--- index/__init__.py
+++ index/__init__.py
@@ -6,7 +6,7 @@ from .utils import import_module
 from .config import Config
 from .core import app
 
+import_module('mounts')
 import_module('commands')
 import_module('events')
 import_module('responses')
-import_module('mounts')
