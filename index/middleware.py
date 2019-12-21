9b0a2458cbedfc767fddc321f5c5cedec68ccb3a
abersheeran <me@abersheeran.com>
2019-12-21 13:57:18 +0800

    change: MiddlewareMixin

--- index/middleware.py
+++ index/middleware.py
@@ -5,24 +5,23 @@ from starlette.responses import Response
 
 from .concurrency import keepasync
 from .responses import automatic
+from .types import HTTPFunc
 
 
 class MiddlewareMixin(metaclass=keepasync("process_request", "process_response")):
 
     ChildMiddlewares: typing.Iterable[typing.Callable] = ()
 
-    def __init__(self, get_response: typing.Callable) -> None:
+    def __init__(self, get_response: HTTPFunc) -> None:
         self.get_response = self.mount_middleware(get_response)
 
-    def mount_middleware(self, get_response: typing.Callable) -> typing.Callable:
+    def mount_middleware(self, get_response: HTTPFunc) -> HTTPFunc:
         for base_middleware in self.ChildMiddlewares:
             get_response = base_middleware(get_response)
         return get_response
 
     async def __call__(self, request: Request) -> Response:
-        response = None
-        if hasattr(self, "process_request"):
-            response = await self.process_request(request)
+        response = await self.process_request(request)
 
         if response is None:
             response = await self.get_response(request)
@@ -31,6 +30,12 @@ class MiddlewareMixin(metaclass=keepasync("process_request", "process_response")
             response = (response,)
         response = automatic(*response)
 
-        if hasattr(self, "process_response"):
-            response = await self.process_response(request, response)
+        response = await self.process_response(request, response)
+
+        return response
+
+    async def process_request(self, request: Request) -> typing.Union[None, typing.Any]:
+        """Must return None, otherwise return the value as the result of this request."""
+
+    async def process_response(self, request: Request, response: Response) -> Response:
         return response
--- index/types.py
+++ index/types.py
@@ -1,8 +1,13 @@
 import typing
 
+from starlette.requests import Request
+from starlette.responses import Response
+
 # WSGI: view PEP3333
 Environ = typing.MutableMapping[str, typing.Any]
 StartResponse = typing.Callable[[str, typing.Iterable[typing.Tuple[str, str]]], None]
 WSGIApp = typing.Callable[
     [Environ, StartResponse], typing.Iterable[typing.Union[str, bytes]]
 ]
+
+HTTPFunc = typing.Callable[[Request], Response]
