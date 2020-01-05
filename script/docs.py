e466c98c7a9460822ab4ac3a5eb729ba432fa7d7
abersheeran <me@abersheeran.com>
2020-01-05 23:58:39 +0800

    add custon domain to docs site

--- script/docs.py
+++ script/docs.py
@@ -8,6 +8,7 @@ rmtree(os.path.join(here, "site"), ignore_errors=True)
 os.system("pipenv run mkdocs build")
 
 os.chdir(os.path.join(here, "site"))
+os.system("echo index-py.abersheeran.com > CNAME")  # Custom domain name
 os.system("git init")
 os.system("git remote add origin https://github.com/abersheeran/index.py")
 os.system("git checkout -B gh-pages")
