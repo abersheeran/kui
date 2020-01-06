8096168f0c786ac3545e6ee0e98cd480d3c9887b
abersheeran <me@abersheeran.com>
2020-01-06 16:49:11 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -62,15 +62,15 @@ class UploadCommand(Command):
         pass
 
     def run(self):
-        # self.status("Removing previous builds…")
-        # rmtree(os.path.join(here, "dist"), ignore_errors=True)
-        # rmtree(os.path.join(here, "build"), ignore_errors=True)
+        self.status("Removing previous builds…")
+        rmtree(os.path.join(here, "dist"), ignore_errors=True)
+        rmtree(os.path.join(here, "build"), ignore_errors=True)
 
-        # self.status("Building Source and Wheel (universal) distribution…")
-        # os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        # self.status("Uploading the package to PyPI via Twine…")
-        # os.system("twine upload dist/*")
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
 
         self.status("Pushing git tags…")
         os.system("git tag v{0}".format(get_version("index")))
