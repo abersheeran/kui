68027d6d2c76cb1c5608079c0be95886acec086c
abersheeran <me@abersheeran.com>
2020-07-06 12:44:20 +0800

    0.11.0

--- indexpy/__version__.py
+++ indexpy/__version__.py
@@ -1,3 +1,3 @@
-VERSION = (0, 10, 9)
+VERSION = (0, 11, 0)
 
 __version__ = ".".join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='index.py',
-    version='0.10.9',
+    version='0.11.0',
     description='An easy-to-use asynchronous web framework based on ASGI.',
     python_requires='==3.*,>=3.6.0',
     project_urls={
