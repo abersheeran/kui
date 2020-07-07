c068b29dc5a7a7eddd1584866592104e04b33584
abersheeran <me@abersheeran.com>
2020-07-07 18:24:35 +0800

    fix type check

--- indexpy/types.py
+++ indexpy/types.py
@@ -1,5 +1,11 @@
+import sys
 from types import TracebackType
 from typing import Any, Type, Tuple, Union, Callable, Iterable, MutableMapping, Optional
+
+if sys.version_info[:2] < (3, 8):
+    from typing_extensions import TypedDict
+else:
+    from typing import TypedDict
 
 from starlette.types import *
 
@@ -30,3 +36,12 @@ Environ = MutableMapping[str, Any]
 StartResponse = Callable[[str, Iterable[Tuple[str, str]], Optional[ExcInfo]], None]
 
 WSGIApp = Callable[[Environ, StartResponse], Iterable[Union[str, bytes]]]
+
+
+class FactoryClass(TypedDict):
+    """
+    `Index().factory_class` type
+    """
+
+    http: typing.Type[Request]
+    websocket: typing.Type[WebSocket]
