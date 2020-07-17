ded71afae1f6144c8e1484c6a63c56f4e5d7d4e0
abersheeran <me@abersheeran.com>
2020-07-17 18:06:45 +0800

    delete templates from config

--- tests/example/example/__init__.py
+++ tests/example/example/__init__.py
@@ -1,6 +1,8 @@
+from indexpy import Index
+
+app = Index(templates=["templates", "other_templates"])
+
 from . import events
 from . import exceptions
 from . import mounts
 from . import responses
-
-from indexpy import app
