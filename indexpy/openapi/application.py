5d26500b645b384b6eff9c5f7bb0e83da9bb4ffb
abersheeran <me@abersheeran.com>
2020-10-14 16:49:31 +0800

    Fix OpenAPI docs generate from pydantic

--- indexpy/openapi/application.py
+++ indexpy/openapi/application.py
@@ -8,7 +8,7 @@ from ..applications import Index
 from ..http.responses import HTMLResponse, JSONResponse, YAMLResponse
 from ..routing import HttpRoute
 from ..types import Literal, TypedDict
-from .schema import schema_parameters, schema_request_body, schema_response
+from .schema import schema_parameter, schema_request_body, schema_response
 
 Tag = TypedDict("Tag", {"description": str, "paths": Sequence[str]})
 
@@ -50,33 +50,39 @@ class OpenAPI:
                 else:
                     self.path2tag[path] = [tag_name]
 
-    def _generate_paths(self, app: Index) -> Dict[str, Any]:
+    def _generate_paths(self, app: Index, definitions: dict) -> Dict[str, Any]:
         result = {}
 
         for path_format, endpoint in app.router.http_tree.iterator():
-            path_docs = self._generate_path(endpoint, path_format)
-            if path_docs:
-                result[path_format] = path_docs
+            path_docs = self._generate_path(endpoint, path_format, definitions)
+            if not path_docs:
+                continue
+            result[path_format] = path_docs
 
         return result
 
-    def _generate_path(self, view: Any, path: str) -> Dict[str, Any]:
+    def _generate_path(self, view: Any, path: str, definitions: dict) -> Dict[str, Any]:
         result = {}
         if hasattr(view, "__methods__"):
             for method in view.__methods__:  # type: ignore
                 if method == "OPTIONS":
                     continue
                 method = method.lower()
-                method_docs = self._generate_method(getattr(view, method), path)
-                if method_docs:
-                    result[method] = method_docs
+                method_docs = self._generate_method(
+                    getattr(view, method), path, definitions
+                )
+                if not method_docs:
+                    continue
+                result[method] = method_docs
         elif hasattr(view, "__method__"):
-            method_docs = self._generate_method(view, path)
+            method_docs = self._generate_method(view, path, definitions)
             if method_docs:
                 result[view.__method__.lower()] = method_docs
         return result
 
-    def _generate_method(self, func: object, path: str) -> Dict[str, Any]:
+    def _generate_method(
+        self, func: object, path: str, definitions: dict
+    ) -> Dict[str, Any]:
         result: Dict[str, Any] = {}
 
         doc = func.__doc__
@@ -88,53 +94,51 @@ class OpenAPI:
                     "description": "\n".join(doc.splitlines()[1:]).strip(),
                 }
             )
-
+        # generate params schmea
         params = getattr(func, "__params__", {})
-        result["parameters"] = schema_parameters(
-            params.get("path"),
-            params.get("query"),
-            params.get("header"),
-            params.get("cookie"),
+        parameters = (
+            schema_parameter(params.get("path"), "path")
+            + schema_parameter(params.get("query"), "query")
+            + schema_parameter(params.get("header"), "header")
+            + schema_parameter(params.get("cookie"), "cookie")
         )
-        if not result["parameters"]:
-            del result["parameters"]
-
-        result["requestBody"] = schema_request_body(params.get("body"))
-        if not result["requestBody"]:
-            del result["requestBody"]
-
-        try:
-            resps = getattr(func, "__resps__")
-        except AttributeError:
-            pass
-        else:
-            result["responses"] = {}
-            for status, content in resps.items():
-                result["responses"][int(status)] = {
-                    "description": content["description"],
-                }
-                if content["model"] is not None:
-                    result["responses"][status]["content"] = schema_response(
-                        content["model"]
-                    )
-            if not result["responses"]:
-                del result["responses"]
-
-        if result and path in self.path2tag:  # has openapi docs, add tags
+        if parameters:
+            result["parameters"] = parameters
+        # generate request body schmea
+        request_body, _definitions = schema_request_body(params.get("body"))
+        if request_body:
+            result["requestBody"] = request_body
+        definitions.update(_definitions)
+        # generate responses schmea
+        resps = getattr(func, "__resps__", {})
+        responses: Dict[int, Any] = {}
+        for status, content in resps.items():
+            _ = responses[int(status)] = {"description": content["description"]}
+            if content["model"] is None:
+                continue
+            _["content"], _definitions = schema_response(content["model"])
+            definitions.update(_definitions)
+        if responses:
+            result["responses"] = responses
+        # set path tags
+        if result and path in self.path2tag:
             result["tags"] = self.path2tag[path]
 
         return result
 
     def create_docs(self, request: Request) -> dict:
         openapi: dict = deepcopy(self.openapi)
+        definitions: Dict[str, Any] = {}
         openapi["servers"] = [
             {
                 "url": f"{request.url.scheme}://{request.url.netloc}",
                 "description": "Current server",
             }
         ]
-        openapi["paths"] = self._generate_paths(cast(Index, request["app"]))
-
+        openapi["paths"] = self._generate_paths(
+            cast(Index, request["app"]), definitions
+        )
+        openapi["definitions"] = definitions
         return openapi
 
     @property
--- indexpy/openapi/schema.py
+++ indexpy/openapi/schema.py
@@ -1,55 +1,23 @@
 from copy import deepcopy
-from typing import Any, Dict, List, Optional, Sequence, Union
+from typing import Any, Dict, List, Tuple, Optional, Union
 
 import yaml
 from pydantic import BaseModel
 
+from ..types import Literal
 from .types import File
 
 
-def replace_definitions(schema: Dict[str, Any]) -> Dict[str, Any]:
-    """
-    replace $ref
-    """
-    schema = deepcopy(schema)
-
-    if schema.get("definitions") is not None:
-
-        def replace(value: Union[str, Sequence[Any], Dict[str, Any]]) -> None:
-            if isinstance(value, str):
-                return
-            elif isinstance(value, Sequence):
-                for _value in value:
-                    replace(_value)
-            elif isinstance(value, Dict):
-                for _name in tuple(value.keys()):
-                    if _name == "$ref":
-                        define_schema = schema
-                        for key in value["$ref"][2:].split("/"):
-                            define_schema = define_schema[key]
-                        # replace ref and del it
-                        value.update(define_schema)
-                        del value["$ref"]
-                    else:
-                        replace(value[_name])
-
-        replace(schema["definitions"])
-        replace(schema["properties"])
-        del schema["definitions"]
-
-    return schema
-
-
-def schema_parameter(m: Optional[BaseModel], position: str) -> List[Dict[str, Any]]:
-    """
-    position: "path", "query", "header", "cookie"
-    """
+def schema_parameter(
+    m: Optional[BaseModel],
+    position: Literal["path", "query", "header", "cookie"],
+) -> List[Dict[str, Any]]:
     result = []
 
     if m is not None:
-        _schemas = replace_definitions(m.schema())
+        _schemas = deepcopy(m.schema())
         properties: Dict[str, Any] = _schemas["properties"]
-        _schemas["required"] = _schemas.get("required") or []
+        required = _schemas.get("required", ())
 
         for name, schema in properties.items():  # type: str, Dict[str, str]
             result.append(
@@ -57,48 +25,36 @@ def schema_parameter(m: Optional[BaseModel], position: str) -> List[Dict[str, An
                     "in": position,
                     "name": name,
                     "description": schema.pop("description", ""),
-                    "required": name in _schemas["required"],  # type: ignore
+                    "required": name in required,  # type: ignore
                     "schema": schema,
                 }
             )
     return result
 
 
-def schema_parameters(
-    path: BaseModel = None,
-    query: BaseModel = None,
-    header: BaseModel = None,
-    cookie: BaseModel = None,
-) -> List[Dict[str, Any]]:
-
-    return (
-        schema_parameter(path, "path")
-        + schema_parameter(query, "query")
-        + schema_parameter(header, "header")
-        + schema_parameter(cookie, "cookie")
-    )
-
-
-def schema_request_body(body: BaseModel = None) -> Optional[Dict[str, Any]]:
+def schema_request_body(body: BaseModel = None) -> Tuple[Optional[Dict], Dict]:
     if body is None:
-        return None
+        return None, {}
 
-    _schema = {"schema": replace_definitions(body.schema())}
+    _schema: Dict = deepcopy(body.schema())
+    definitions = _schema.pop("definitions", {})
 
     for field in body.__fields__.values():
         if issubclass(field.type_, File):
             return {
                 "required": True,
-                "content": {"multipart/form-data": _schema},
-            }
+                "content": {"multipart/form-data": {"schema": _schema}},
+            }, definitions
 
     return {
         "required": True,
-        "content": {"application/json": _schema},
-    }
+        "content": {"application/json": {"schema": _schema}},
+    }, definitions
 
 
-def schema_response(model: Union[BaseModel, str]) -> Dict[str, Any]:
+def schema_response(model: Union[BaseModel, str]) -> Tuple[Dict, Dict]:
     if isinstance(model, str):
-        return yaml.safe_load(model.strip())
-    return {"application/json": {"schema": replace_definitions(model.schema())}}
+        return yaml.safe_load(model.strip()), {}
+    schema = deepcopy(model.schema())
+    definitions = schema.pop("definitions", {})
+    return {"application/json": {"schema": schema}}, definitions
