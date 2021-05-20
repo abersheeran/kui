c169eabe87e6aaae584a6935a009efbd79368b27
abersheeran <me@abersheeran.com>
2021-05-20 23:00:47 +0800

    -

--- indexpy/routing/__init__.py
+++ indexpy/routing/__init__.py
@@ -1,11 +1,9 @@
-from .extensions.fileroutes import FileRoutes
 from .routes import HttpRoute, NoMatchFound, NoRouteFound, Prefix, Routes, SocketRoute
 
 __all__ = [
     "Routes",
     "HttpRoute",
     "SocketRoute",
-    "FileRoutes",
     "Prefix",
     "NoMatchFound",
     "NoRouteFound",
---
+++ indexpy/routing/extensions/__init__.py
@@ -0,0 +1,5 @@
+from .file import FileRoutes
+
+__all__ = [
+    "FileRoutes",
+]
