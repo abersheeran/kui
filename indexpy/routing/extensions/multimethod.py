5688b77ba406eed5fab8b7e30255ddab1c7eb6d1
abersheeran <me@abersheeran.com>
2021-06-06 00:14:58 +0800

    -

--- indexpy/routing/extensions/multimethod.py
+++ indexpy/routing/extensions/multimethod.py
@@ -1,18 +1,37 @@
 import typing
 
 from indexpy.routing.routes import BaseRoute, HttpRoute, Routes
-
+from indexpy.views import HttpView
 
 _RoutesSelf = typing.TypeVar("_RoutesSelf", bound="MultimethodRoutes")
 
 
 class MultimethodRoutes(Routes):
+    def __init__(
+        self,
+        *iterable: typing.Union[BaseRoute, typing.Iterable[BaseRoute]],
+        namespace: str,
+        http_middlewares: typing.Sequence[typing.Any],
+        socket_middlewares: typing.Sequence[typing.Any],
+        base_class: typing.Type[HttpView] = HttpView
+    ) -> None:
+        super().__init__(
+            *iterable,
+            namespace=namespace,
+            http_middlewares=http_middlewares,
+            socket_middlewares=socket_middlewares,
+        )
+        self.base_class = base_class
+
     def append(self: _RoutesSelf, route: BaseRoute) -> _RoutesSelf:
-        if isinstance(route, HttpRoute) and hasattr(route.endpoint, "__method__"):
-            for r in self:
-                if r.path == route.path and isinstance(route, HttpRoute):
-                    if not hasattr(r, "__method__"):
-                        raise RuntimeError("")  # TODO
-                    else:
-                        pass
+        if hasattr(route.endpoint, "__methods__"):
+            raise TypeError("MultimethodRoutes not allow use class-base view.")
         return super().append(route)
+
+    def __iter__(self) -> typing.Iterator[BaseRoute]:
+        result: typing.List[BaseRoute] = []
+        for route in self._list:
+            if isinstance(route, HttpRoute):
+                pass  # TODO
+
+        return iter(result)
