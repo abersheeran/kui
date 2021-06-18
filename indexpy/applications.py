c1dbf66aecdccdbf18a90cd1f801b2a4f01c07c7
abersheeran <me@abersheeran.com>
2021-06-19 00:37:25 +0800

    More precise Type hint

--- indexpy/applications.py
+++ indexpy/applications.py
@@ -5,7 +5,18 @@ import dataclasses
 import inspect
 import sys
 import traceback
-from typing import Any, Callable, Dict, List, Optional, Type, TypeVar, Union
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Type,
+    TypeVar,
+    Union,
+)
 
 if sys.version_info[:2] < (3, 8):
     from typing_extensions import Literal
@@ -16,18 +27,20 @@ from baize.asgi import Receive, Scope, Send
 from baize.utils import cached_property
 
 from .debug import DebugMiddleware
-from .exceptions import ExceptionContextManager, HTTPException
+from .exceptions import ErrorView, ExceptionContextManager, HTTPException
 from .requests import HttpRequest, WebSocket, request_var, websocket_var
 from .responses import convert_response
 from .routing.routes import BaseRoute, NoMatchFound, Router
 from .templates import BaseTemplates
 from .utils import State
 
+NoArgumentCallable = Union[Callable[[], Any], Callable[[], Awaitable[Any]]]
+
 
 @dataclasses.dataclass
 class Lifespan:
-    on_startup: List[Callable[[], Any]] = dataclasses.field(default_factory=list)
-    on_shutdown: List[Callable[[], Any]] = dataclasses.field(default_factory=list)
+    on_startup: List[NoArgumentCallable] = dataclasses.field(default_factory=list)
+    on_shutdown: List[NoArgumentCallable] = dataclasses.field(default_factory=list)
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         """
@@ -67,7 +80,7 @@ class FactoryClass:
     websocket: Type[WebSocket] = WebSocket
 
 
-CallableObject = TypeVar("CallableObject", bound=Callable)
+T_NoArgumentCallable = TypeVar("T_NoArgumentCallable", bound=NoArgumentCallable)
 
 
 class Index:
@@ -76,10 +89,10 @@ class Index:
         *,
         debug: bool = False,
         templates: Optional[BaseTemplates] = None,
-        on_startup: List[Callable] = [],
-        on_shutdown: List[Callable] = [],
-        routes: List[BaseRoute] = [],
-        exception_handlers: Dict[Union[int, Type[Exception]], Callable] = {},
+        on_startup: List[NoArgumentCallable] = [],
+        on_shutdown: List[NoArgumentCallable] = [],
+        routes: Iterable[BaseRoute] = [],
+        exception_handlers: Dict[int | Type[Exception], ErrorView] = {},
         factory_class: FactoryClass = FactoryClass(),
     ) -> None:
         self.__dict__["debug"] = debug
@@ -101,28 +114,26 @@ class Index:
         self.app_with_debug.debug = bool(value)
 
     def add_exception_handler(
-        self,
-        exc_class_or_status_code: Union[int, Type[Exception]],
-        handler: Callable,
+        self, exc_class_or_status_code: int | Type[Exception], handler: ErrorView
     ) -> None:
         self.exception_contextmanager.add_exception_handler(
             exc_class_or_status_code, handler
         )
 
     def exception_handler(
-        self, exc_class_or_status_code: Union[int, Type[Exception]]
-    ) -> Callable[[CallableObject], CallableObject]:
-        def decorator(func: CallableObject) -> CallableObject:
+        self, exc_class_or_status_code: int | Type[Exception]
+    ) -> Callable[[ErrorView], ErrorView]:
+        def decorator(func: ErrorView) -> ErrorView:
             self.add_exception_handler(exc_class_or_status_code, func)
             return func
 
         return decorator
 
-    def on_startup(self, func: CallableObject) -> CallableObject:
+    def on_startup(self, func: T_NoArgumentCallable) -> T_NoArgumentCallable:
         self.lifespan.on_startup.append(func)
         return func
 
-    def on_shutdown(self, func: CallableObject) -> CallableObject:
+    def on_shutdown(self, func: T_NoArgumentCallable) -> T_NoArgumentCallable:
         self.lifespan.on_shutdown.append(func)
         return func
 
