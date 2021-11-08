5d7cb5c2de20ef633473bdca58d06306e62a7fed
abersheeran <me@abersheeran.com>
2021-11-08 09:46:50 +0800

    Fixed concurrency.py type check error

--- indexpy/concurrency.py
+++ indexpy/concurrency.py
@@ -3,11 +3,19 @@ from __future__ import annotations
 import asyncio
 import contextvars
 import functools
-from typing import Any, AsyncGenerator, Awaitable, Callable, Iterator, TypeVar, Union
+import typing
+from typing import (
+    Any,
+    AsyncGenerator,
+    Awaitable,
+    Callable,
+    Generator,
+    Iterator,
+    TypeVar,
+    overload,
+)
 
-from indexpy.utils.inspect import is_async_gen_callable
-
-from .utils import is_coroutine_callable, is_gen_callable
+from .utils import is_async_gen_callable, is_coroutine_callable, is_gen_callable
 
 T = TypeVar("T")
 
@@ -19,7 +27,7 @@ async def run_in_threadpool(func: Callable[..., T], *args: Any, **kwargs: Any) -
     loop = asyncio.get_running_loop()
     child = functools.partial(func, *args, **kwargs)
     context = contextvars.copy_context()
-    return await loop.run_in_executor(None, context.run, child)
+    return typing.cast(T, await loop.run_in_executor(None, context.run, child))
 
 
 class _StopIteration(Exception):
@@ -42,24 +50,36 @@ async def iterate_in_threadpool(iterator: Iterator[T]) -> AsyncGenerator[T, None
     while True:
         try:
             child = functools.partial(_next, iterator)
-            yield await loop.run_in_executor(None, context.run, child)
+            yield typing.cast(T, await loop.run_in_executor(None, context.run, child))
         except _StopIteration:
             break
 
 
-def always_async(
-    func: Callable[..., T],
-) -> Union[Callable[..., AsyncGenerator[T, None]], Callable[..., Awaitable[T]]]:
+@overload
+def always_async(  # type: ignore
+    func: Callable[..., Generator[T, None, None]]
+) -> Callable[..., AsyncGenerator[T, None]]:
+    ...
+
+
+@overload
+def always_async(func: Callable[..., T]) -> Callable[..., Awaitable[T]]:
+    ...
+
+
+def always_async(func):
     if is_coroutine_callable(func) or is_async_gen_callable(func):
         return func
 
     if is_gen_callable(func):
 
         @functools.wraps(func)
-        async def wrapper(*args, **kwargs):
+        async def wrapper_gen(*args, **kwargs):
             generator = func(*args, **kwargs)
             async for item in iterate_in_threadpool(generator):
                 yield item
+
+        return wrapper_gen
 
     else:
 
@@ -67,4 +87,4 @@ def always_async(
         async def wrapper(*args, **kwargs):
             return await run_in_threadpool(func, *args, **kwargs)
 
-    return wrapper
+        return wrapper
