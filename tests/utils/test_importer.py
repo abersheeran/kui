9cfac018b50120acf481b2f9036535a2a9a28795
abersheeran <me@abersheeran.com>
2024-03-26 13:46:59 +0800

    Update exception handling of imported functions

--- tests/utils/test_importer.py
+++ tests/utils/test_importer.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import pytest
 
-from kui.utils.importer import ImportFromStringError, import_from_string, import_module
+from kui.utils.importer import import_from_string, import_module
 
 
 def test_import_module():
@@ -16,11 +16,11 @@ def test_import_from_string():
         is import_from_string
     )
 
-    with pytest.raises(ImportFromStringError):
+    with pytest.raises(ValueError):
         import_from_string("kui.utils.importer")
 
-    with pytest.raises(ImportFromStringError):
+    with pytest.raises(ImportError):
         import_from_string("kui.utils.xxxxxxxx:import_from_string")
 
-    with pytest.raises(ImportFromStringError):
+    with pytest.raises(AttributeError):
         import_from_string("kui.utils.importer:import_from_string:foo")
